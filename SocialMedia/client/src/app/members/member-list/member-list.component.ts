45c4a5faa6442b26600ee25faf71bc2a840df3fe
sarahchen <sarah.chen@dyedurham.com>
2023-06-26 09:32:18 +1000

    "add member-card child component"

--- SocialMedia/client/src/app/members/member-list/member-list.component.ts
+++ SocialMedia/client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
