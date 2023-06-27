7babc3dc342ad2dcadb34ecc9d9084d98a8e2018
sarahchen <sarah.chen@dyedurham.com>
2023-06-27 12:27:45 +1000

    "interceptor to add auth token hearder to request"

--- SocialMedia/client/src/app/_services/members.service.ts
+++ SocialMedia/client/src/app/_services/members.service.ts
@@ -12,21 +12,11 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + '/v1/users',this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + '/v1/users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + '/v1/' + username, this.getHttpOptions());
+    return this.http.get<Member>(this.baseUrl + '/v1/' + username);
   }
 
-  getHttpOptions(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    return{
-      headers: new HttpHeaders({
-         Authorization: 'Bearer ' + user.token
-      })
-    }
-  }
 }
