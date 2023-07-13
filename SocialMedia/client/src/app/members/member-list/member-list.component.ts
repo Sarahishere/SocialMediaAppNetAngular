d1379b620571a1327a80b7b95f2588292744fa34
sarahchen <sarah.chen@dyedurham.com>
2023-07-13 15:00:08 +1000

    "load members in member service instead"

--- SocialMedia/client/src/app/members/member-list/member-list.component.ts
+++ SocialMedia/client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,17 +9,12 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members: Member[] = [];
+  members$: Observable<Member[]> | undefined;
 
   constructor(private memberService: MembersService){}
   
   ngOnInit(): void {
-    this.loadMembers();
+    this.members$ = this.memberService.getMembers();
   }
 
-  loadMembers() {
-    this.memberService.getMembers().subscribe({
-       next: members => this.members = members
-    })
-  }
 }
